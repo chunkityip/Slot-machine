1ddf00f27020ae7361e6cf1038e8ae90c09851b7
chunkityip <alex0917lfo@hotmail.com>
2023-12-13 19:37:51 -0500

    Adding while loop

--- project.js
+++ project.js
@@ -1,9 +1,23 @@
 
 const prompt = require("prompt-sync")();
 
-const deposit = () => {
-    const depositAmount = parseFloat(prompt("Enter your deposit amount: $"));
-    console.log(`You deposited is : $${depositAmount}`);
-}
 
-deposit();
+//A method call deposit
+const deposit = () => {
+    while (true) {
+        const depositAmount = parseFloat(prompt("Enter your deposit amount: $"));
+        const numberDepositAmount = parseFloat(prompt(depositAmount));
+
+        //In JavaScript , isNaN is a function that stands for "is Not a Number."
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log(
+                "Please enter a valid amount. Amount must be greater than 0."
+                )
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+const depositAmount = deposit();
+console.log("You have deposited: $" + depositAmount);
