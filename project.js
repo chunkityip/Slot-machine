15dfd6b5ff889a0d6eedb491cabdf51bb5f5a7fa
chunkityip <alex0917lfo@hotmail.com>
2023-12-27 14:14:13 -0500

    Finish

--- project.js
+++ project.js
@@ -73,6 +73,7 @@ const spin = () => {
 
     const reels = [[] , [] , []];
     for (let i = 0; i < COLS; i++) {
+        removeEventListener.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -84,8 +85,69 @@ const spin = () => {
     return reels;
 };
 
-spin();
+const transpose = (reels) => {
+    const rows = [];
+    for (let j = 0; j < COLS; j++) {
+        rows[i].push(reels[j][i]);
+    }
+};
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance , numberOfLines);
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i , symbol] of row.entries()) {
+            rowString += symbol;
+            if (i != row.length - 1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+};
+
+
+const getWinnings = (rows , bet , lines) => {
+    let winnings = 0;
+
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+};
+
+const game = () => {
+    while(true) {
+        console.log("Welcome to the Slot Machine!");
+        console.log("You have a balance of $" + balance);
+        let balance = deposit();
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance , numberOfLines);
+        balance -= bet * numberOfLines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows , bet , numberOfLines);
+        balance += winnings;
+        console.log(`You won ${winnings}`);
+
+        if (balance <= 0) {
+            console.log("You ran out of money!");
+            break;
+        }
+
+        const playAgain = prompt("Do you want to play again (y/n)? ");
+        if (playAgain != "y") break;
+    }
+}
